827346366851eb4b05d678eee7e12edbcfde544f
cuban <cubannn@gmail.com>
2020-12-17 20:08:39 +0200

    fix files loop

--- tests/test_html_syntax.py
+++ tests/test_html_syntax.py
@@ -1,6 +1,6 @@
 import os
-
-for filename in os.listdir("$GITHUB_WORKSPACE"):
+# print(os.environ.get('GITHUB_WORKSPACE'))
+for filename in os.listdir(os.environ.get('GITHUB_WORKSPACE') + "/tests"):
     if filename.endswith(".asm") or filename.endswith(".py"):
         print(os.path.join(directory, filename))
         # continue
